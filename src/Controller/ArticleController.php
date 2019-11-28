a3d33eb18fdf1e418d173833ace65be8347ef2af
s15211 <s15211@pjwstk.edu.pl>
2019-11-28 10:17:55 +0100

    SwiftMailer

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,9 +11,22 @@ class ArticleController extends AbstractController
     /**
      * @Route("/")
      */
-    public function homepage()
+    public function homepage(\Swift_Mailer $mailer)
     {
-        return  $this->render('base.html.twig');
+        $message = (new \Swift_Message('Aktywuj konto!'))
+            ->setFrom('driven3twork@gmail.com')
+            ->setTo('s15211@pjwstk.edu.pl')
+            ->setBody('Kod aktywacji konta to : xxxxxxx');
+        $mailer->send($message);
+        return  $this->render('article/index.html.twig');
+    }
+
+    /**
+     * @Route("/contact")
+     */
+    public function contact()
+    {
+        return  $this->render('article/contact.html.twig.html.twig');
     }
 
     /**
